04ae572f256a9ba28e0b7cde47f00193e359b145
Nemka201 <nemka201@gmail.com>
2023-05-04 17:51:29 -0300

    Commit prueba

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -13,9 +13,9 @@ export class AuthService {
 
   constructor(private HttpClient: HttpClient) { }
   public Nuevo(nuevoUsuario: NuevoUsuario): Observable<any>{
-    return this.HttpClient.post<any>(this.authURL + 'nuevo',nuevoUsuario)
+    return this.HttpClient.post<any>(this.authURL + '/nuevo',nuevoUsuario)
   }
   public Login(loginUsuario: LoginUsuario): Observable<JwtDto>{
-    return this.HttpClient.post<JwtDto>(this.authURL + 'login', loginUsuario);
+    return this.HttpClient.post<JwtDto>(this.authURL + '/login', loginUsuario);
   }
 }
